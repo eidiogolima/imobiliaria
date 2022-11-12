41cd37cca95cc6b3ba111a92f6d0a96702c09072
*Diogo <diogolimakas@hotmail.com>
2022-11-12 10:22:50 -0300

    fix update header-mobile-function open and close

--- src/JS/Global/Header.js
+++ src/JS/Global/Header.js
@@ -1,70 +1,87 @@
 import React from 'react';
 import styles from '../../CSS/Header.module.css';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import Accont from '../../assets/icons-main/acc.svg';
 import AccontImg from '../../assets/icons-main/accontimg.svg';
 
 const Header = () => {
+  
+
+  React.useEffect(()=>{
+    const btn = document.querySelector('.menuMobile');
+    console.log(btn);
+    const links = document.querySelectorAll('a');
+
+    links.forEach((link) => {
+      link.addEventListener('click', () => {
+        btn.classList.remove(styles.active);
+      });
+    });
+  })
+
   return (
     <header id={styles.headerBg}>
       <div className="container" id={styles.header}>
-
         <button
           onClick={({ target }) => target.classList.toggle(styles.active)}
-          id={styles.menuMobile}
-          className={styles.active}
-        >
-        </button>
+          id={`${styles.menuMobile}`}
+          className="menuMobile"
+        ></button>
 
         <ul id={styles.accontDisableMobile}>
-          <Link to="entrar" className="font-1-s">
+          <NavLink to="entrar" className="font-1-s">
             Entrar
-          </Link>
-          <Link to="cadastrar" className="font-1-s">
+          </NavLink>
+          <NavLink to="cadastrar" className="font-1-s">
             Cadastrar
-          </Link>
+          </NavLink>
         </ul>
-        <Link to="conta" id={styles.contaMobile}>
+        <NavLink to="conta" id={styles.contaMobile}>
           <img src={AccontImg} alt="" />
-        </Link>
-        <Link to="home" id={styles.logoHeader}>
+        </NavLink>
+        <NavLink to="home" id={styles.logoHeader}>
           <h1>LOGO</h1>
-        </Link>
+        </NavLink>
 
         <nav id={styles.nav}>
           <ul>
-
-            <Link id = {styles.menuText} className='font-2-xs cor-11' style={{color:'var(--cor-11)', fontWeight:'500'}}>Menu</Link>
-            <Link to="vender" className="font-1-s">
+            <NavLink
+              id={styles.menuText}
+              className="font-2-xs cor-11"
+              style={{ color: 'var(--cor-11)', fontWeight: '500' }}
+            >
+              Menu
+            </NavLink>
+            <NavLink to="vender" className="font-1-s">
               Vender
-            </Link>
-            <Link to="servicos" className="font-1-s">
+            </NavLink>
+            <NavLink to="servicos" className="font-1-s">
               Serviços
-            </Link>
-            <Link to="ajuda" className="font-1-s">
+            </NavLink>
+            <NavLink to="ajuda" className="font-1-s">
               Ajuda
-            </Link>
-            <Link to="whatsapp" className="font-1-s">
+            </NavLink>
+            <NavLink to="whatsapp" className="font-1-s">
               WhatsApp
-            </Link>
+            </NavLink>
 
             <ul id={styles.accontDisable}>
-              <Link to="entrar" className="font-1-s">
+              <NavLink to="entrar" className="font-1-s">
                 Entrar
-              </Link>
-              <Link to="cadastrar" className="font-1-s">
+              </NavLink>
+              <NavLink to="cadastrar" className="font-1-s">
                 Cadastrar
-              </Link>
+              </NavLink>
             </ul>
           </ul>
         </nav>
         <ul id={styles.accont}>
-          <Link to="entrar" className="font-1-s">
+          <NavLink to="entrar" className="font-1-s">
             Entrar
-          </Link>
-          <Link to="cadastrar" className="font-1-s">
+          </NavLink>
+          <NavLink to="cadastrar" className="font-1-s">
             Cadastrar
-          </Link>
+          </NavLink>
           <img src={Accont} alt="" />
         </ul>
       </div>
