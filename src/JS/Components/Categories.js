050059084e114f6a603d757314821c95c4e4b0a2
*Diogo <diogolimakas@hotmail.com>
2022-11-23 11:02:56 -0300

    fix-modal+att-home

---
+++ src/JS/Components/Categories.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from '../../CSS/Components/Categories.module.css'
+
+const Categories = () => {
+  return (
+    <section className='container' id = {styles.categories} >
+      
+    </section>
+  )
+}
+
+export default Categories
--- src/JS/Home/Home.js
+++ src/JS/Home/Home.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Head from '../Global/Head';
-
+import styles from '../../CSS/Pages/Home.module.css';
 import Modal from '../Components/Modal';
 
 function Home() {
@@ -10,7 +10,12 @@ function Home() {
         title="Home | Imóvel"
         description="Alugar casa, apartamento ou lote nunca foi tão fácil, tem imóvel ? coloque para vender em poucos cliques "
       />
-      <Modal/>
+
+      <section id={styles.homeBg}>
+        <div id={styles.home}>
+          <Modal />
+        </div>
+      </section>
     </>
   );
 }
